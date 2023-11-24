8050bcc31222908a009e3654affc6f0e7bcec1cc
Nao <naolal30@hotmail.com>
2023-11-24 17:38:09 -0500

    I expected something different.

--- project/controller/brokers.js
+++ project/controller/brokers.js
@@ -7,14 +7,6 @@ const router = express.Router()
 const app = express();
 app.use(bodyParser.json());
 
-app.set('poop-engine', 'ejs');
-app.use(express.static(__dirname+'/poop'));
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-
-
 const uri = "mongodb+srv://naolal30:ConnectdatabasetoWebstorm100.@cluster0.ttfusik.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 
@@ -27,20 +19,15 @@ try{
 
 router.get('/edit/:id', async (req, res) => {
     const broker = await get1Broker(client, req.params.id);
-//console.log(broker.name)
     res.render('../project/views/broker/editBroker.ejs', {broker: broker})
-//res.send("patoe")
-
 })
 
 router.get('/show/:id', async (req, res) => {
     const broker = await get1Broker(client, req.params.id);
     res.render('../project/views/broker/showBroker.ejs', {broker: broker})
-    
 })
 
 router.get('/brokerListings/:id', async (req, res) => {
-    
     const houses = client.db("soen_341").collection("houses").find({broker: new ObjectId(req.params.id)}).toArray();
     res.render('../project/views/broker/brokerListings.ejs', {houses: houses})
 });
--- project/controller/listings.js
+++ project/controller/listings.js
@@ -10,13 +10,6 @@ const router  = express.Router();
 const app = express();
 app.use(bodyParser.json());
 
-app.set('poop-engine', 'ejs');
-app.use(express.static(__dirname+'/poop'));
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-
 const uri = "mongodb+srv://naolal30:ConnectdatabasetoWebstorm100.@cluster0.ttfusik.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 
@@ -32,7 +25,7 @@ router.get('/requestU/:id', async (req, res) => {
     res.render('../project/views/listings/requestU.ejs', {houses: houses})
 });
 
-router.post("/buy_rentUU", async (req, res) => {
+router.post("/buy_rentU", async (req, res) => {
     let location = req.body.location.toLowerCase();
     let minPrice = req.body.minPrice;
     let maxPrice = req.body.maxPrice;
@@ -53,6 +46,31 @@ router.post("/buy_rentUU", async (req, res) => {
     if (houseArr1[1] === true) message = "No results found";
 
     res.render('../project/views/listings/buy_rentU.ejs', {houses: houseArr1[0], message: message}); // opens localhost on index.html
+});
+router.post("/buy_rentB", async (req, res) => {
+    let location = req.body.location.toLowerCase();
+    let minPrice = req.body.minPrice;
+    let maxPrice = req.body.maxPrice;
+    let bath = req.body.bath;
+    let beds = req.body.beds;
+    let yearBuild = req.body.yearBuild;
+    let floors = req.body.floors;
+    let garage = req.body.garage;
+    let prop = req.body.prop;
+    let furnished = req.body.furnished;
+    let extra = req.body.extra;
+    let propsize = req.body.propsize;
+    let listingType = req.body.listing;
+    let time = req.body.time;
+
+    const houseArr1 = await buy_rentJS(location, minPrice, maxPrice, bath, beds, yearBuild, floors, garage, prop, furnished, extra, propsize, listingType, time, client);
+
+    let message = "";
+    if (houseArr1[1] === true) message = "No results found";
+
+
+    res.render('../project/views/listings/buy_rentB.ejs', {houses: houseArr, message: message});
+    res.render('../project/views/listings/buy_rentB.ejs', {houses: houseArr1[0], message: message}); // opens localhost on index.html
 });
 
 router.get('/showU/:id', async (req, res) => {
